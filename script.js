d94d6ef0036b288e88fb0b0f94e130a619218daf
StarostaX60fps <starosta.original@gmail.com>
2024-11-15 22:51:13 +0100

    2.4

---
+++ script.js
@@ -0,0 +1,23 @@
+// Получаем элементы
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+const modalWindow = document.getElementById('modalWindow');
+
+// Открыть модальное окно
+openModalBtn.addEventListener('click', () => {
+    modalWindow.style.display = 'flex'; // Показываем модальное окно
+});
+
+// Закрыть модальное окно
+closeModalBtn.addEventListener('click', () => {
+    modalWindow.style.display = 'none'; // Скрываем модальное окно
+});
+
+// Закрытие модального окна при клике вне его содержимого
+window.addEventListener('click', (event) => {
+    if (event.target === modalWindow) {
+        modalWindow.style.display = 'none';
+    }
+});
+
+const myModalAlternative = new bootstrap.Modal('#myModal', options)
